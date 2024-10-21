7a5bb3722e6f117c1b269095fc0ecddeaf0d0665
mirecek <pavlimi2@uhk.cz>
2024-10-21 05:26:10 +0200

    5th comm

--- src/rasterizer/LineSnapper.java
+++ src/rasterizer/LineSnapper.java
@@ -12,13 +12,13 @@ public class LineSnapper {
             if (Math.abs(dy) < Math.abs(dx) / 2) {
                 return new Point(current.getX(), start.getY());
             } else {
-                return new Point(current.getX(), start.getY() + dx);
+                return new Point(current.getX(), start.getY() + (dx > 0 ? dy : -dy));
             }
         } else {
             if (Math.abs(dx) < Math.abs(dy) / 2) {
                 return new Point(start.getX(), current.getY());
             } else {
-                return new Point(start.getX() + dy, current.getY());
+                return new Point(start.getX() + (dy > 0 ? dx : -dx), current.getY());
             }
         }
     }
